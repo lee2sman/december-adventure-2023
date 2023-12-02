e61015015703d32826b30cdfa370999d293ddb4d
lee2sman <leetusman@gmail.com>
2023-12-02 03:42:09 -0500

    initial commit

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#site builder
+
+pandoc -s index.md -o index.html -H assets/includes/header.html -A assets/includes/footer.html -c assets/css/main.css --metadata title="December Adventure"
