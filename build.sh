35a680e75272b1a199a944db254f899924fef084
lee2sman <leetusman@gmail.com>
2023-12-03 20:21:56 -0500

    added lang='en' to the build/html output cli build.sh call

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #site builder
 
-pandoc -s index.md -o index.html -H assets/includes/header.html -A assets/includes/footer.html -c assets/css/main.css --metadata title="December Adventure"
+pandoc -s index.md -o index.html -H assets/includes/header.html -A assets/includes/footer.html -c assets/css/main.css -V lang=en --metadata title="December Adventure"
